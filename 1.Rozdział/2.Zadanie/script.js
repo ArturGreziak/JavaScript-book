bae52d11e06491c782ec92070827c8720cac0477
Artur Gręziak <alex4448@wp.pl>
2022-02-22 23:13:27 +0100

    FizzBuzz

---
+++ 1.Rozdział/2.Zadanie/script.js
@@ -0,0 +1,6 @@
+/*
+FizzBuzz
+Napisz program drukujący za pomocą funkcji console.log wszystkie liczby od 1 do 100 z dwoma wyjątkami.Zamiast liczb podzielnych przez 3 wydrukuj słowo Fizz, a zamiast liczb podzielnych przez 5 wydrukuj słowo Buzz.
+Gdy już uda Ci się napisać taki program, zmień go tak, aby drukował napis FizzBuzz zamiast liczb podzielnych zarówno przez 3, jak i przez 5(i nadal drukował słowa Fizz i Buzz dla liczb podzielnych tylko przez jedną z nich).
+(Jest to zadanie wykorzystywane podczas rozmów o pracę dla programistów, przez które podobno odpada spory odsetek kandydatów.Jeśli więc udało Ci się je rozwiązać, to masz powody do zadowolenia).
+*/
