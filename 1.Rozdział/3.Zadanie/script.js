982ff4769f217ae49d2084054bf9889631a34a14
Artur Gręziak <alex4448@wp.pl>
2022-02-22 23:20:37 +0100

    Chessboard

---
+++ 1.Rozdział/3.Zadanie/script.js
@@ -0,0 +1,25 @@
+/*
+Szachownica
+Napisz program tworzący łańcuch reprezentujący siatkę elementów o wymiarach 8×8, używając 
+znaków nowego wiersza do oddzielania poszczególnych wierszy. Na każdej pozycji siatki może 
+być spacja albo znak #. Znaki te powinny tworzyć szachownicę.Przekazanie tego łańcucha do 
+funkcji console.log powinno dawać efekt podobny do poniższego:
+
+# # # #
+
+ # # # #
+
+# # # #
+
+ # # # #
+
+# # # #
+
+ # # # #
+
+# # # #
+
+ # # # #
+
+Po utworzeniu programu generującego taki wzór zdefiniuj wiązanie size = 8 i tak zmodyfikuj program, aby dla dowolnej wartości tego wiązania drukował planszę o określonych wymiarach.
+*/
