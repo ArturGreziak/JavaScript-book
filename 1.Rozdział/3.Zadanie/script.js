dbd1cd049b753fb38622e61b8e9fcc2b5eeb3a87
Artur Gręziak <alex4448@wp.pl>
2022-02-28 21:33:10 +0100

    chessboard

--- 1.Rozdział/3.Zadanie/script.js
+++ 1.Rozdział/3.Zadanie/script.js
@@ -24,20 +24,16 @@ funkcji console.log powinno dawać efekt podobny do poniższego:
 Po utworzeniu programu generującego taki wzór zdefiniuj wiązanie size = 8 i tak zmodyfikuj program, 
 aby dla dowolnej wartości tego wiązania drukował planszę o określonych wymiarach.
 */
-// const pionowoHasz = 4;
-// const poziomoHasz = 4;
-// for (let i = 0; i < pionowoHasz; i++){
-//     console.log(i);
-// let str = "";
-//     for (let j = 0; j < poziomoHasz; j++) {
-//         str += `#  \n`;
-// }
-//     console.log(str);
-// }
+// pętle zagnieżdżone są jak normalne pętle, po prostu jedna z instrukcji pętli jest inna pętla
+// najprostsze scanariusze to np.chodzenie po jakiejś planszy 2d / tabelce / liście obiektów które mają w sobie inne listy
+// pętlę wewnętrzną można wrzucić do jakiejś funkcji co często ma sens,
+// zagnieżdżone pętle to często sygnał że kod jest nieczytelny,
+// rzadko kiedy kod z dużym poziomem zagnieżdżeń jest sensowny, a każda pętla to dodatkowy poziom zagnieżdżenia
+// ale czasem coś można napisać na szybko albo jak przekazywanie stanu pomiędzy poziomami byłoby zbyt upierdliwe a scenariusz jest w miarę prosty
 
 let size = 8;
 for (let i = 1; i <= size; i++) {
-    var str = '';
+    let str = '';
     for (let j = 1; j <= size; j++)
         str += j % 2 ? '#' : ' ';
     if (i % 2 == 0)
