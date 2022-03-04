7373624d00deda053e205f4491e65fa308501852
Artur Gręziak <alex4448@wp.pl>
2022-03-04 23:26:09 +0100

    sum

--- 3.Rozdział/1.Zadanie/script.js
+++ 3.Rozdział/1.Zadanie/script.js
@@ -16,3 +16,12 @@
 // }
 // console.log(range(1,10))
 
+function sum(arr) {
+    let suma = 0;
+    for (let i = 0; i < arr.length; i++) {
+        suma += arr[i];
+    }
+    return suma;
+}
+
+console.log(sum([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
