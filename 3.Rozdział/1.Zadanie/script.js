948fb35261b5dd0287b5ceb0a80622bff56bcb27
Artur Gręziak <alex4448@wp.pl>
2022-03-05 13:48:26 +0100

    sum of a range of numbers

--- 3.Rozdział/1.Zadanie/script.js
+++ 3.Rozdział/1.Zadanie/script.js
@@ -9,6 +9,7 @@ function range(start, end) {
     }
     return arr;
 }
+console.log(range(1, 10)) // (10) [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 // Następnie napisz funkcję sum pobierającą tablicę liczb i zwracającą ich sumę.Wykonaj przykładowy program i sprawdź, czy rzeczywiście zwróci 55.
 
@@ -19,6 +20,7 @@ function sum(arr) {
     }
     return sum;
 }
+console.log(sum([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])); // 55
 
 // Jako dodatkowe ćwiczenie zmodyfikuj funkcję range tak, aby opcjonalnie przyjmowała trzeci argument określający rozmiar „kroku” po wartościach do budowania tablicy. 
 // Jeśli argument ten nie zostanie zdefiniowany, wartości tablicy powinny być zwiększane o jeden, czyli tak jak wcześniej. 
@@ -26,9 +28,9 @@ function sum(arr) {
 
 function range(start, end, step = 1) {
     const arr = [];
-    for (let i = start; i <= end; i += -step) {
+    for (let i = start; step && (step > 0 ? i <= end : i >= end); i += step) {
         arr.push(i)
     }
     return arr;
 }
-console.log(range(1, 10, -2))  //(5) [1, 3, 5, 7, 9]
+console.log(range(10, -10, -1));  // (21)[10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, -1, -2, -3, -4, -5, -6, -7, -8, -9, -10]
